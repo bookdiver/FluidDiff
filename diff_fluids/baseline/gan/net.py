5a4477a91ed87533ea8cbd125c611f3ba72bdf0a
Rickyang2233 <vbd402@alumni.ku.dk>
2023-01-23 18:28:06 +0100

    add GAN baseline

--- diff_fluids/baseline/gan/net.py
+++ diff_fluids/baseline/gan/net.py
@@ -3,63 +3,85 @@ from collections import OrderedDict
 import torch
 import torch.nn as nn
 
+def get_activation(activation: str):
+    if activation == 'leaky_relu':
+        return nn.LeakyReLU(0.2, inplace=True)
+    elif activation == 'tanh':
+        return nn.Tanh()
+    else:
+        raise ValueError(f'Invalid activation: {activation}')
+
 
 class UNetGenerator(nn.Module):
     def __init__(self,
                  in_channels: int,
                  out_channels: int,
-                 init_features: int=64):
+                 init_features: int=64,
+                 activation: str='leaky_relu',):
         super().__init__()
-        self.in_z_dims = in_z_dims
-        self.in_y_channels = in_y_channels
-        self.out_channels = out_channels
-        self.init_features = init_features
-
-        self.init_z_conv = self._init_z_conv()
-        self.init_y_conv = self._init_y_conv()
-
-        self.res_blocks = nn.Sequential(
-            ResBlock(init_features * 4 * 2, init_features * 4 * 4),
-            ResBlock(init_features * 4 * 4, init_features * 4 * 4),
-            ResBlock(init_features * 4 * 4, init_features * 4 * 2),
-            ResBlock(init_features * 4 * 2, init_features * 4),
+        self.init_conv = nn.Sequential(
+            OrderedDict([
+                ('conv', nn.Conv2d(in_channels, init_features, kernel_size=3, stride=2, padding=1, bias=False)),
+                ('norm', nn.BatchNorm2d(init_features)),
+                ('act', get_activation(activation))
+            ])
         )
-        self.up_blocks = nn.Sequential(
-            self._upsample(init_features * 4, init_features * 2),
-            self._upsample(init_features * 2, init_features),
-            self._upsample(init_features, init_features // 2),
-        )
-        self.out_conv = self._out_conv()
 
-    
-    
-    def _out_conv(self):
+        self.down_path = nn.Sequential(
+            self._down_block(init_features, init_features * 2),
+            self._down_block(init_features * 2, init_features * 4),
+            self._down_block(init_features * 4, init_features * 8),
+            self._down_block(init_features * 8, init_features * 8)
+        )
+
+        self.up_path = nn.Sequential(
+            self._up_block(init_features * 8 * 2, init_features * 8),
+            self._up_block(init_features * 8 * 2, init_features * 4),
+            self._up_block(init_features * 4 * 2, init_features * 2),
+            self._up_block(init_features * 2 * 2, init_features)
+        )
+        
+        self.out_conv = nn.Sequential(
+            OrderedDict([
+                ('conv', nn.ConvTranspose2d(init_features * 2, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1, bias=False)),
+                ('norm', nn.BatchNorm2d(out_channels)),
+                ('act', get_activation(activation))
+            ])
+        )
+
+
+    @staticmethod
+    def _down_block(in_channels: int, out_channels: int, activation: str='leaky_relu'):
         return nn.Sequential(
             OrderedDict([
-                ('conv', nn.Conv2d(self.init_features // 2, self.out_channels, kernel_size=3, stride=1, padding=1, bias=False)),
-                ('act', nn.Tanh())
+                ('conv', nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1, bias=False)),
+                ('norm', nn.BatchNorm2d(out_channels)),
+                ('act', get_activation(activation))
             ])
         )
     
-
     @staticmethod
-    def _down_block(in_channels: int, out_channels: int):
+    def _up_block(in_channels: int, out_channels: int, activation: str='leaky_relu'):
         return nn.Sequential(
             OrderedDict([
                 ('conv', nn.ConvTranspose2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1, bias=False)),
                 ('norm', nn.BatchNorm2d(out_channels)),
-                ('act', nn.LeakyReLU(0.2, inplace=True))
+                ('act', get_activation(activation))
             ])
         )
     
-    def forward(self, z: torch.Tensor, y: torch.Tensor):
-        x1 = self.init_z_conv(z)
-        x2 = self.init_y_conv(y)
-        x = torch.cat([x1, x2], dim=1)
-        for res_block in self.res_blocks:
-            x = res_block(x)
-        x = self.up_blocks(x)
-        return self.out_conv(x)
+    def forward(self, x: torch.Tensor):
+        hs = []
+        h = self.init_conv(x)
+        hs.append(h)
+        for block in self.down_path:
+            h = block(h)
+            hs.append(h)
+        
+        for block in self.up_path:
+            h = block(torch.cat([h, hs.pop()], dim=1))
+        
+        return self.out_conv(torch.cat([h, hs.pop()], dim=1))
 
 
 def weights_init_normal(m):
@@ -73,44 +95,40 @@ def weights_init_normal(m):
 
 
 class Discriminator(nn.Module):
-    def __init__(self, in_x_channels, in_y_channels):
+    def __init__(self, in_channels: int, init_features: int=64, activation: str='leaky_relu'):
         super(Discriminator, self).__init__()
 
-        def discriminator_block(in_filters, out_filters, normalization=True):
-            """Returns downsampling layers of each discriminator block"""
-            layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
-            if normalization:
-                layers.append(nn.InstanceNorm2d(out_filters))
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
-            return layers
-
         self.model = nn.Sequential(
-            *discriminator_block(in_x_channels+in_y_channels, 32, normalization=False),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            *discriminator_block(32, 64),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Flatten(),
-            nn.Linear(64 * 4 * 4, 1),
-            nn.Sigmoid(),
+            self._block(in_channels, init_features, kernel_size=4, stride=2, padding=1, activation=activation),
+            self._block(init_features, init_features * 2, kernel_size=4, stride=2, padding=1, activation=activation),
+            self._block(init_features * 2, init_features * 4, kernel_size=4, stride=2, padding=1, activation=activation),
+            self._block(init_features * 4, init_features * 8, kernel_size=4, stride=1, padding=1, activation=activation),
+            nn.Conv2d(init_features * 8, 1, kernel_size=4, stride=1, padding=1, bias=False)
+        )
+    
+    @staticmethod
+    def _block(in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int, activation: str):
+        return nn.Sequential(
+            OrderedDict([
+                ('conv', nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)),
+                ('norm', nn.BatchNorm2d(out_channels)),
+                ('act', get_activation(activation))
+            ])
         )
 
-    def forward(self, img_A, img_B):
-        # Concatenate image and condition image by channels to produce input
-        img_input = torch.cat((img_A, img_B), 1)
-        return self.model(img_input)
+    def forward(self, x:torch.Tensor, y: torch.Tensor):
+        input = torch.cat((x, y), 1)
+        return self.model(input)
 
 def _test():
     x = torch.randn(4, 2, 64, 64)
     y = torch.randn(4, 2, 64, 64)
-    z = torch.randn(4, 128, 1, 1)
-    generator = Generator(in_z_dims=128,
-                            in_y_channels=2,
-                            out_channels=2)
-    discriminator = Discriminator(in_x_channels=2,
-                                  in_y_channels=2)
+    generator = UNetGenerator(in_channels=2,
+                              out_channels=2)
+    discriminator = Discriminator(in_channels=4)
     print(f"The number of parameters in generator: {sum(p.numel() for p in generator.parameters())}")
     print(f"The number of parameters in discriminator: {sum(p.numel() for p in discriminator.parameters())}")
-    G_out = generator(z, y)
+    G_out = generator(x)
     print(G_out.shape)
     D_out = discriminator(x, y)
     print(D_out.shape)
--- diff_fluids/baseline/gan/train.py
+++ diff_fluids/baseline/gan/train.py
@@ -4,14 +4,15 @@ import logging
 import yaml
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader, random_split
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 import matplotlib
 import matplotlib.pyplot as plt
 
 from utils import CGANDataset
-from net import Discriminator, Generator, weights_init_normal
+from net import Discriminator, UNetGenerator, weights_init_normal
 
 matplotlib.use('Agg')
 
@@ -20,6 +21,7 @@ parse = argparse.ArgumentParser(description='Conditional GAN baseline training')
 parse.add_argument('--device', type=int, default=0, help='device to use (default 0)')
 parse.add_argument('--debug', action='store_true', help='debug mode, default False')
 parse.add_argument('--savename', type=str, default='default', help='name of the experiment, default "default"')
+parse.add_argument('--start_epoch', type=int, default=1, help='start epoch, default 1')
 
 
 class Trainer:
@@ -31,19 +33,17 @@ class Trainer:
         self.device = args.device
         self.debug = args.debug
         self.savename = args.savename
+        self.start_epoch = args.start_epoch
         self.configs = configs
 
         self.eval_interval = configs['recording_params']['eval_interval']
         self.tb_writer_root = configs['recording_params']['tb_writer_root']
         self.model_save_root = configs['recording_params']['model_save_root']
 
-        if self.debug:
-            logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(asctime)s:%(message)s')
-        else:
-            logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(asctime)s:%(message)s')
+        if not self.debug:
             self.tb_writer = SummaryWriter(log_dir=self.tb_writer_root + self.savename +'/')
 
-        self.generator = Generator(**configs['model']['generator']).to(self.device)
+        self.generator = UNetGenerator(**configs['model']['generator']).to(self.device)
         self.discriminator = Discriminator(**configs['model']['discriminator']).to(self.device)
         self.generator.apply(weights_init_normal)
         self.discriminator.apply(weights_init_normal)
@@ -55,6 +55,7 @@ class Trainer:
         self.G_lr = configs['training_params']['G_learning_rate']
         self.D_lr = configs['training_params']['D_learning_rate']
         self.batch_size = configs['training_params']['batch_size']
+        self.lambda_GAN = configs['training_params']['lambda_GAN']
 
         self.train_dl = DataLoader(train_dataset, 
                                     batch_size=self.batch_size, 
@@ -67,10 +68,11 @@ class Trainer:
                                     num_workers=8,
                                     pin_memory=False)
 
-        self.optimizerG = torch.optim.Adam(self.generator.parameters(), lr=self.G_lr)
-        self.optimizerD = torch.optim.Adam(self.discriminator.parameters(), lr=self.D_lr)
+        self.optimizerG = torch.optim.Adam(self.generator.parameters(), lr=self.G_lr, betas=(0.5, 0.999))
+        self.optimizerD = torch.optim.Adam(self.discriminator.parameters(), lr=self.D_lr, betas=(0.5, 0.999))
 
-        self.loss_fn = nn.BCELoss()
+        self.GAN_loss = nn.MSELoss()
+        self.patch_loss = nn.L1Loss()
 
         logging.info("Trainer initialized")
         logging.info(f"Training set size: {len(self.train_dl.dataset)}")
@@ -82,92 +84,100 @@ class Trainer:
         real_label = 1.
         fake_label = 0.
 
-        for epoch in range(1, 1 + self.n_epochs):
+        if self.start_epoch != 1:
+            self.generator.load_state_dict(torch.load(self.model_save_root + self.savename + f'_generator.pt'))
+            self.discriminator.load_state_dict(torch.load(self.model_save_root + self.savename + '_discriminator.pt'))
+
+        for epoch in range(self.start_epoch, 1 + self.n_epochs):
 
             # Training
             self.generator.train()
             self.discriminator.train()
 
+            cum_train_loss_G = 0.
+            cum_train_loss_D = 0.
+
             pbar_train = tqdm(self.train_dl)
             for i, data in enumerate(pbar_train):
-                # Train discriminator
-                self.discriminator.zero_grad()
-                x = data['x'].to(self.device)
-                y = data['y'].to(self.device)
-                b = x.shape[0]
-                
-                label = torch.full((b,), real_label, device=self.device, dtype=torch.float)
-                output = self.discriminator(x, y).reshape(-1)
-                D_x = output.mean().item()
-                loss_D_real = self.loss_fn(output, label)
-                loss_D_real.backward()
 
-                z = torch.randn(b, 128, 1, 1, device=self.device)
-                fake = self.generator(z, y)
-                label.fill_(fake_label)
-                output = self.discriminator(fake.detach(), y).reshape(-1)
-                D_G_z1 = output.mean().item()
-                loss_D_fake = self.loss_fn(output, label)
-                loss_D_fake.backward()
+                real_x = data['x'].to(self.device)
+                real_y = data['y'].to(self.device)
 
-                loss_D = loss_D_real + loss_D_fake
-                self.optimizerD.step()
-                
-                # Train generator
-                self.generator.zero_grad()
-                label.fill_(real_label)
-                output = self.discriminator(fake, y).reshape(-1)
-                D_G_z2 = output.mean().item()
-                loss_G = self.loss_fn(output, label)
+                # Train Generator
+                self.optimizerG.zero_grad()
+
+                # GAN loss
+                fake_x = self.generator(real_y)
+                pred_fake = self.discriminator(fake_x, real_y)
+                loss_GAN =  self.GAN_loss(pred_fake, Variable(torch.full_like(pred_fake, real_label), requires_grad=False))
+                loss_patch = self.patch_loss(fake_x, real_x)
+
+                loss_G = loss_GAN + self.lambda_GAN * loss_patch
+                cum_train_loss_G += loss_G.item()
                 loss_G.backward()
+
                 self.optimizerG.step()
 
+                # Train Discriminator
+                self.optimizerD.zero_grad()
+
+                # Real loss
+                pred_real = self.discriminator(real_x, real_y)
+                loss_real = self.GAN_loss(pred_real, Variable(torch.full_like(pred_real, real_label), requires_grad=False))
+
+                # Fake loss
+                pred_fake = self.discriminator(fake_x.detach(), real_y)
+                loss_fake = self.GAN_loss(pred_fake, Variable(torch.full_like(pred_fake, fake_label), requires_grad=False))
+
+                # Total loss
+                loss_D = (loss_real + loss_fake) / 2
+                cum_train_loss_D += loss_D.item()
+                loss_D.backward()
+
+                self.optimizerD.step()
+
                 pbar_train.set_description(f"Training [{epoch:>{epoch_len}}/{self.n_epochs:>{epoch_len}}] | " +
-                                           f"G Loss: {loss_G.item():.4f} " +
-                                           f"D Loss: {loss_D.item():.4f} " +
-                                           f"D(x): {D_x:.4f} " +
-                                           f"D(G(z)): {D_G_z1:.4f}/{D_G_z2:.4f}")
+                                           f"Avg G Loss: {cum_train_loss_G / (i+1):.4f} " +
+                                           f"Avg D Loss: {cum_train_loss_D / (i+1):.4f} ")
                 if hasattr(self, 'tb_writer'):
                     self.tb_writer.add_scalars('Train Loss', 
                                                 {"Generator": loss_G.item(), "Discriminator": loss_D.item()}, epoch * len(self.train_dl) + i)
-                    self.tb_writer.add_scalars('Performance', 
-                                                {"D(x)": D_x, "D(G(z))": D_G_z2}, epoch * len(self.train_dl) + i)
 
             # Validation
             self.generator.eval()
             self.discriminator.eval()
+            cum_val_loss_G = 0.
+            cum_val_loss_D = 0.
             with torch.no_grad():
                 pbar_val = tqdm(self.val_dl)
                 for i, data in enumerate(pbar_val):
-                    x = data['x'].to(self.device)
-                    y = data['y'].to(self.device)
-                    b = x.shape[0]
+                    real_x = data['x'].to(self.device)
+                    real_y = data['y'].to(self.device)
 
-                    label = torch.full((b,), real_label, device=self.device, dtype=torch.float)
-                    output = self.discriminator(x, y).reshape(-1)
-                    D_x = output.mean().item()
+                    # GAN loss
+                    fake_x = self.generator(real_y)
+                    pred_fake = self.discriminator(fake_x, real_y)
+                    loss_GAN = self.GAN_loss(pred_fake, Variable(torch.full_like(pred_fake, real_label), requires_grad=False))
+                    loss_patch = self.patch_loss(fake_x, real_x)
 
-                    loss_D_real = self.loss_fn(output, label)
-                    
-                    z = torch.randn(b, 128, 1, 1, device=self.device)
-                    fake = self.generator(z, y)
-                    label.fill_(fake_label)
-                    output = self.discriminator(fake.detach(), y).reshape(-1)
-                    D_G_z1 = output.mean().item()
-                    loss_D_fake = self.loss_fn(output, label)
+                    loss_G = loss_GAN + self.lambda_GAN * loss_patch
+                    cum_val_loss_G += loss_G.item()
 
-                    loss_D = loss_D_real + loss_D_fake
+                    # Real loss
+                    pred_real = self.discriminator(real_x, real_y)
+                    loss_real = self.GAN_loss(pred_real, Variable(torch.full_like(pred_real, real_label), requires_grad=False))
 
-                    label.fill_(real_label)
-                    output = self.discriminator(fake, y).reshape(-1)
-                    D_G_z2 = output.mean().item()
-                    loss_G = self.loss_fn(output, label)
+                    # Fake loss
+                    pred_fake = self.discriminator(fake_x.detach(), real_y)
+                    loss_fake = self.GAN_loss(pred_fake, Variable(torch.full_like(pred_fake, fake_label), requires_grad=False))
+
+                    # Total loss
+                    loss_D = (loss_real + loss_fake) / 2
+                    cum_val_loss_D += loss_D.item()
 
                     pbar_val.set_description(f"Validation [{epoch:>{epoch_len}}/{self.n_epochs:>{epoch_len}}] | " +
-                                             f"G Loss: {loss_G.item():.4f} " +
-                                             f"D Loss: {loss_D.item():.4f} " +
-                                             f"D(x): {D_x:.4f} " +
-                                             f"D(G(z)): {D_G_z1:.4f}/{D_G_z2:.4f}")
+                                             f"Avg G Loss: {cum_val_loss_G / (i+1):.4f} " +
+                                             f"Avg D Loss: {cum_val_loss_D / (i+1):.4f} ")
             
             # Testing
             if epoch % self.eval_interval == 0 or epoch == self.n_epochs:
@@ -176,8 +186,7 @@ class Trainer:
                     sample = next(iter(self.val_dl))
                     x0 = sample['x'][:4]
                     y = sample['y'][:4].to(self.device)
-                    z = torch.randn(4, 128, 1, 1, device=self.device)
-                    x_pred = self.generator(z, y)
+                    x_pred = self.generator(y)
                 fig, ax = plt.subplots(4, 4, figsize=(16, 16))
                 x0 = x0.detach().numpy()
                 x_pred = x_pred.detach().cpu().numpy()
@@ -194,7 +203,8 @@ class Trainer:
                     self.tb_writer.add_figure("Sampling on Test set", fig, epoch)
                 plt.close(fig)
             if not self.debug:
-                torch.save(self.generator.state_dict(), self.model_save_root + f'{self.savename}.pth')
+                torch.save(self.generator.state_dict(), self.model_save_root + self.savename + '_generator.pt')
+                torch.save(self.discriminator.state_dict(), self.model_save_root + self.savename + '_discriminator.pt')
 
         logging.info('Training finished')
         self.tb_writer.close()
